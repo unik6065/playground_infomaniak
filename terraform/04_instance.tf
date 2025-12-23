8a6e96e37fc931603351bec3af5b60cb7f500e08
Romain Isoz <isozromain@gmail.com>
2025-12-23 16:31:23 +0100

    [terraform] Stick to openstack good practices

--- terraform/04_instance.tf
+++ terraform/04_instance.tf
@@ -9,6 +9,7 @@ resource "openstack_networking_floatingip_v2" "floating_1" {
 
 resource "openstack_networking_port_v2" "port_openvpn" {
   network_id = module.network_dev.network_id
+  security_group_ids = [openstack_networking_secgroup_v2.openvpn.id, openstack_networking_secgroup_v2.ssh.id]
 
   fixed_ip {
     subnet_id = module.network_dev.subnet_id
@@ -20,8 +21,6 @@ resource "openstack_compute_instance_v2" "openvpn" {
   image_id    = var.instance_image_id
   flavor_name = var.instance_flavor_name
   metadata    = var.metadatas
-  security_groups = [openstack_networking_secgroup_v2.openvpn.name,
-  openstack_networking_secgroup_v2.ssh.name, "default"]
   key_pair = openstack_compute_keypair_v2.ssh_public_key.name
   network {
     port = openstack_networking_port_v2.port_openvpn.id
