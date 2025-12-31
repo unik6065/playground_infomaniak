574b0b63ec0a5ee47c84fcc06d7190603466850a
Romain Isoz <isozromain@gmail.com>
2025-12-31 11:31:43 +0100

    [ansible] add dns role and push dnsmasq on all server

--- terraform/01_vpn/04_instance.tf
+++ terraform/01_vpn/04_instance.tf
@@ -3,12 +3,13 @@ module "openvpn" {
   instance_count                 = 1
   instance_name                  = "openvpn"
   instance_key_pair              = openstack_compute_keypair_v2.ssh_public_key.name
-  instance_security_groups_ids   = [openstack_networking_secgroup_v2.openvpn.id, openstack_networking_secgroup_v2.ssh.id]
+  instance_security_groups_ids   = [openstack_networking_secgroup_v2.openvpn.id, openstack_networking_secgroup_v2.ssh.id, openstack_networking_secgroup_v2.consul.id]
   instance_network_internal_name = var.network_internal_dev
   instance_network_internal_id   = module.network_dev.network_id
   instance_network_external_name = var.network_external_name
   instance_network_external_id   = var.network_external_id
   instance_ssh_key               = var.ssh_public_key_default_user
+  instance_internal_fixed_ip     = "10.0.1.1"
   public_floating_ip             = true
   metadatas = {
     environment = "dev"
