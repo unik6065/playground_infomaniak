91c0448355ec3ac019daaceed8b76d490dc203c7
Romain Isoz <isozromain@gmail.com>
2025-12-31 13:25:43 +0100

    [ansible] add base roles to uniform machines

---
+++ terraform/02_infrastructure/10_ansible.tf
@@ -0,0 +1,15 @@
+
+
+resource "null_resource" "ansible_infrastructure" {
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = <<-EOT
+      sleep 20s;
+      ANSIBLE_CONFIG=../../ansible/ansible.cfg ansible-playbook -u ansible -i ../../ansible/openstack.yml --private-key ~/.ssh/info ../../ansible/infrastructure_base.yml;
+      ANSIBLE_CONFIG=../../ansible/ansible.cfg ansible-playbook -u ansible -i ../../ansible/openstack.yml --private-key ~/.ssh/info ../../ansible/infrastructure_consul.yml;
+    EOT
+  }
+    depends_on = [module.consul]
+}
