a2215fbfe7846803339c9cafb72433720ebdd116
Romain Isoz <isozromain@gmail.com>
2026-02-14 10:39:41 +0100

    [ansible] - Add node_exporter to project

--- terraform/02_infrastructure/10_ansible.tf
+++ terraform/02_infrastructure/10_ansible.tf
@@ -10,6 +10,7 @@ resource "null_resource" "ansible_infrastructure" {
       ${var.ANSIBLE_ENV_VARS} ${var.ANSIBLE_COMMAND} ${var.default_user} ${var.ANSIBLE_OPTIONS} -e users_default_account=${var.default_user} ../../ansible/infrastructure_base.yml;
       ${var.ANSIBLE_ENV_VARS} ${var.ANSIBLE_COMMAND} ${var.default_user} ${var.ANSIBLE_OPTIONS}  ../../ansible/infrastructure_consul.yml;
       ${var.ANSIBLE_ENV_VARS} ${var.ANSIBLE_COMMAND} ${var.default_user} ${var.ANSIBLE_OPTIONS}  ../../ansible/infrastructure_consul_openvpn.yml;
+      ${var.ANSIBLE_ENV_VARS} ${var.ANSIBLE_COMMAND} ${var.default_user} ${var.ANSIBLE_OPTIONS}  ../../ansible/infrastructure_consul_services.yml;
     EOT
   }
   depends_on = [module.consul]
