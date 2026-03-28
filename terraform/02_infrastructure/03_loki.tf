5303d10f0fccd8dd5e29d4655043211e9796c011
Romain Isoz <isozromain@gmail.com>
2026-03-28 10:40:01 +0100

    add loki

---
+++ terraform/02_infrastructure/03_loki.tf
@@ -0,0 +1,17 @@
+module "loki" {
+  source                            = "../modules/instances"
+  instance_count                    = 1
+  instance_name                     = "loki"
+  instance_key_pair                 = "default_key"
+  instance_security_groups_ids      = [data.terraform_remote_state.vpn.outputs.security_group_all_internal_id]
+  instance_network_internal_name    = var.network_internal_dev
+  instance_network_internal_id      = data.terraform_remote_state.vpn.outputs.network_dev_id
+  instance_ssh_key                  = var.ssh_public_key_default_user
+  instance_default_user             = var.default_user
+  instance_volumes_count            = 1  
+  metadatas = {
+    environment = "dev",
+    app         = "loki"
+  }
+  instance_subnet_id = data.terraform_remote_state.vpn.outputs.network_dev_subnet_id
+}
